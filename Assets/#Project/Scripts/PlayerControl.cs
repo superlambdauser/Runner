0ae147fe48069729a54d6ebe8f40a3ee05daebc3
deepthroadocus <sockpuppetmastaz@proton.me>
2025-09-11 12:04:26 +0200

    Partie 2 : Jump

--- Assets/#Project/Scripts/PlayerControl.cs
+++ Assets/#Project/Scripts/PlayerControl.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+
 public class PlayerControl : MonoBehaviour
 {
-
-    //public InputActions actions;
-
     public InputActionAsset actions;
     public float speed = 1f;
+
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] Collider road;
     private InputAction xAxis;
+    private InputAction yAxis;
+    private Rigidbody rb;
+    private bool isJumping = false;
+    private float xMove;
+    private float yMove;
 
     void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         xAxis = actions.FindActionMap("CubeActionsMap").FindAction("XAxis");
+        yAxis = actions.FindActionMap("CubeActionsMap").FindAction("YAxis");
     }
 
     void OnEnable()
@@ -27,21 +37,56 @@ public class PlayerControl : MonoBehaviour
         actions.FindActionMap("CubeActionsMap").Disable();
     }
 
-    void Update()
+
+    void Update() //Inputs, UI, Cameras
     {
-        MoveX();
-        MoveZ();
+        xMove = xAxis.ReadValue<float>();
+        yMove = yAxis.ReadValue<float>();
+
+        Jump();
     }
 
-    private void MoveX()
+    void FixedUpdate() //Physics (Rigidbody)
     {
-        float xMove = xAxis.ReadValue<float>();
-        transform.position += speed * Time.deltaTime * xMove * transform.right;
-
+        Move();
     }
 
-    private void MoveZ()
+    // private void MoveX()
+    // {
+    //     float xMove = xAxis.ReadValue<float>();
+    //     transform.position += speed * Time.deltaTime * xMove * transform.right;
+
+    // }
+
+    // private void MoveZ()
+    // {
+    //     transform.position += speed * Time.deltaTime * transform.forward;
+    // }
+
+    void Move()
     {
-        transform.position += speed * Time.deltaTime * transform.forward;
+        Vector3 xzMovement = (transform.right * xMove + transform.forward) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + xzMovement);
+    }
+
+    private void Jump()
+    {
+        if (isJumping == false && yMove > 0)
+        {
+
+            isJumping = true;
+            Debug.Log($"jumping ? {isJumping} -- yMove : {yMove}");
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("check 1 --- Collided with: " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Road"))
+        {
+            Debug.Log("check 2 --- Collided with: " + collision.gameObject.name);
+            isJumping = false;
+        }
     }
 }
